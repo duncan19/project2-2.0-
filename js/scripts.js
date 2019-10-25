7c925b7259b62fd37cb27043bd0097fb1caf49da
Duncan Robbins <duncanrobbins19@gmail.com>
2019-10-25 13:52:29 -0700

    start adding js

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".questions").submit(function(event) {
+    var area =  parseInt($("#music").val());
+    var color =  parseInt($("#pet").val());
+    var pet =  parseInt($("#day").val());
+    var eye = parseInt($("#eyecolor").val());
+    var day =  parseInt($("#age").val())
+
+    $(".thequestions").hide();
+    if (music + day + pet + eyecolor + age === 10) {
+      $("#results").show();
+    } else if (music + day + pet + eyecolor + age  > 10){
+      $("#results2").show();
+    } else {
+      $("#results3").show();
+    }
+
+    event.preventDefault();
+  });
+});
